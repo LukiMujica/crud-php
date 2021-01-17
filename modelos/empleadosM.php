fce32d9ae1d4d57c76bbeabfd15405ba56ef1e85
LukiMujica <lucas150998@gmail.com>
2021-01-16 23:02:43 -0300

    Editar Empleados

--- modelos/empleadosM.php
+++ modelos/empleadosM.php
@@ -43,5 +43,20 @@ class EmpleadosM extends conexionBD{
 
     }
 
+    //Editar empleado
+    static public function EditarEmpleadoM($datosC, $tablaBD){
+
+        $pdo = conexionBD::cBD()->prepare("SELECT id, nombre, apellido, email, puesto, salario FROM $tablaBD WHERE id = :id");
+
+        $pdo -> bindParam(":id", $datosC, PDO::PARAM_INT);
+
+        $pdo -> execute();
+
+        return $pdo->fetch();
+
+        $pdo->close();
+
+    }
+
 
 }
